ce37f237dbd340431a3270d57e54906d28c05d32
flipps <fi.morais.mail@gmail.com>
2018-01-16 23:21:41 -0200

    Added Sass to the project

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './styles.scss';
 
-const App = () => <div>Hey Parcel!</div>
+const App = () => <div className="sass-test">Hey Parcel!</div>
 
 ReactDom.render(<App />, 
   document.getElementById('root')
